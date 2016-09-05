5893d97a00f0353229abde474c350395aea0f3d3
nrhin <nathan@hintonclan.org>
2016-09-05 17:22:59 +0000

    programm sulutions for chapters 3-5

---
+++ nathan-programming-puzzles-5-4.py
@@ -0,0 +1,13 @@
+from random import randint
+
+ninjas = randint(0, 75)
+
+if ninjas < 50:
+    print("Too many")
+if ninjas < 30:
+    print("It will be a strugle, but I can do it")
+if ninjas < 10:
+    print("I can fight those ninjas")
+
+print()
+print(ninjas)
