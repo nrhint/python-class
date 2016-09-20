c30365eee52837a379100f475b73b0f76dcf3040
nrhin <nathan@hintonclan.org>
2016-09-20 02:50:34 +0000

    changed some problems.

--- nathan-programming-puzzles-5-4.py
+++ nathan-programming-puzzles-5-4.py
@@ -4,9 +4,9 @@ ninjas = randint(0, 75)
 
 if ninjas < 50:
     print("Too many")
-if ninjas < 30:
+elif ninjas < 30:
     print("It will be a strugle, but I can do it")
-if ninjas < 10:
+else ninjas < 10:
     print("I can fight those ninjas")
 
 print()
--- nathan-programming-puzzles-6-2.py
+++ nathan-programming-puzzles-6-2.py
@@ -1,8 +1,16 @@
-age = 14
+import random
+age = random.randint(50, 150)
 x = 0
 
-while x != age:
-    x = x + 1
-    if x%2 == 0:
-        print(x)
-    
+if age%2 == 0:
+    while x != age:
+        x = x + 1
+        if x%2 == 0:
+            print(x)
+else:
+    while x != age:
+        x = x + 1
+        if x%2 == 1:
+            print(x)
+if age > 75:
+    print("Dad is %d years old" % age)
